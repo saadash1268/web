421ae2f57cd1e33536e80bb37bc2048b12c2ff0c
Saad Ashraf <saadash1268@gmail.com>
2023-12-16 22:08:43 +0000

    file structure and intro

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,14 @@
+import "./intro.css"
+
+
+const Intro = () => {
+    return (
+        <div className="i">
+            <div className="i-left">left</div>
+            <div className="i-right">right</div>
+        </div>
+    )
+}
+
+
+export default Intro
