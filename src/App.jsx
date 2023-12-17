1747211d1e1e20f961095a38bf683300f1b61434
Saad Ashraf <saadash1268@gmail.com>
2023-12-17 11:49:29 +0000

    about me added but need to change content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import About from "./components/about/About";
 import Intro from "./components/intro/intro";
 
+
 const App = () => {
-  return <div><Intro/></div>;
+  return ( 
+    <div>
+      <Intro/>
+      <About/>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/about/About.jsx
@@ -0,0 +1,20 @@
+import "./about.css";
+
+
+const About = () => {
+    return (
+        <div className="a">
+            <div className="a-left"></div>
+            <div className="a-right">
+                <h1 className="a-title">About Me</h1>
+                <p className="a-sub">Change content, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt.</p>
+                <p className="a-desc">Change content.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt.
+                In hac habitasse platea dictumst. Duis euismod augue vel odio fermentum, vel dictum ex tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt.
+                In hac habitasse platea dictumst. Duis euismod augue vel odio fermentum, vel dictum ex tincidunt.</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default About
