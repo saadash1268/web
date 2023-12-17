f0be370c4bde63d1889dd13233d9ba068f31e357
Saad Ashraf <saadash1268@gmail.com>
2023-12-17 00:34:37 +0000

    intro section at the top underway

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -4,7 +4,14 @@ import "./intro.css"
 const Intro = () => {
     return (
         <div className="i">
-            <div className="i-left">left</div>
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, my name is</h2>
+                    <h1 className="i-name">Saad Ashraf</h1>
+                    <div className="i-title-item"> Full Stack Developer</div>
+                    <div className="i-title-text">Just two sentences here</div>
+                </div>
+            </div>
             <div className="i-right">right</div>
         </div>
     )
