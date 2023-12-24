2afdb42784f9fe396fd36c8fcdf5d02a57ab4948
Saad Ashraf <saadash1268@gmail.com>
2023-12-24 23:41:56 +0000

    added videos of projects but need styinling so they fit in the cards

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,18 +1,16 @@
+import React from "react";
 import "./product.css";
 
-
-
-const Product = () => {
-    return (
-        <div className="p">
-            <div className="p-browser">
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-            </div>
+const Product = ({ videoSrc }) => {
+  return (
+    <div className="p">
+      <div className="p-browser">
+        <div className="p-circle">
+          <video src={videoSrc} width="600" height="300" controls autoPlay={false} />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-
-export default Product
+export default Product;
--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,22 +1,22 @@
+import React from "react";
 import "./productList.css";
-import Product from "../product/Product"
+import Product from "../product/Product";
+import gardenGenieVideo from "../../assets/newCopy of Garden Genie by M.A.S.H.E.D Long.mp4";
+import pokedexVideo from "../../assets/Pokedex.mp4";
 
 const ProductList = () => {
-    return (
-        <div className="pl">
-            <div className="pl=texts">
-                <h1 className="pl-title"> Add a title</h1>
-                <p className="pl-desc">
-                    add some information describing here
-                </p>
-            </div>
-            <div className="pl-list">
-                <Product/>
-                <Product/>
-                <Product/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="pl">
+      <div className="pl=texts">
+        <h1 className="pl-title"> Add a title</h1>
+        <p className="pl-desc">add some information describing here</p>
+      </div>
+      <div className="pl-list">
+        <Product videoSrc={gardenGenieVideo} />
+        <Product videoSrc={pokedexVideo} />
+      </div>
+    </div>
+  );
+};
 
-export default ProductList
+export default ProductList;
