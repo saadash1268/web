60dc0f2662552911ef795b41e52112a3a9196443
Saad Ashraf <saadash1268@gmail.com>
2024-01-01 14:46:10 +0000

    changed some styling for intro:

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -6,10 +6,10 @@ const Intro = () => {
         <div className="i">
             <div className="i-left">
                 <div className="i-wrapper">
-                    <h2 className="i-intro">Hello, my name is</h2>
+                    <h2 className="i-intro">Hey, I'm</h2>
                     <h1 className="i-name">Saad Ashraf</h1>
-                    <div className="i-title-item"> Full Stack Developer</div>
-                    <div className="i-title-text">Creative thinker and problem solver fascinated by the ever-evolving landscape of development. Currently looking for new opportunities</div>
+                    <div className="i-title-item">Full Stack Developer</div>
+                    <div className="i-title-text">A creative thinker and problem solver fascinated by the ever-evolving landscape of development. Currently looking for new opportunities.</div>
                 </div>
             </div>
         </div>
