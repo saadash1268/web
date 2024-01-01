af6ef5ed0b6fdce7cc785469c6e1fcfbff12e1b5
Saad Ashraf <saadash1268@gmail.com>
2024-01-01 14:04:19 +0000

    intro text added

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,14 +5,13 @@ const Intro = () => {
     return (
         <div className="i">
             <div className="i-left">
-                <div className="i-left-wrapper">
+                <div className="i-wrapper">
                     <h2 className="i-intro">Hello, my name is</h2>
                     <h1 className="i-name">Saad Ashraf</h1>
                     <div className="i-title-item"> Full Stack Developer</div>
-                    <div className="i-title-text">Just two sentences here</div>
+                    <div className="i-title-text">Creative thinker and problem solver fascinated by the ever-evolving landscape of development. Currently looking for new opportunities</div>
                 </div>
             </div>
-            <div className="i-right">right</div>
         </div>
     )
 }
