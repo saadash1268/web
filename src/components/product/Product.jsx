6bed8657adcb8ee65c71c26e06c603970e567d51
Saad Ashraf <saadash1268@gmail.com>
2024-01-03 14:36:50 +0000

    changed garden genie image and now added the garden genie video as a pop up with a button but will need stylin

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,12 +1,14 @@
-import React from "react";
 import "./product.css";
 
-const Product = ({ imageSrc }) => {
+const Product = ({ imageSrc, videoSrc }) => {
   return (
     <div className="p">
       
         <div className="p-circle">
             <img src={imageSrc} alt="product" style={{width:"100%", height:"100%"}}/>
+        </div>
+        <div className="Video">
+          <video src={videoSrc}></video>
         </div>
       
     </div>
--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,10 +1,23 @@
-import React from "react";
 import "./productList.css";
 import Product from "../product/Product";
 import gardenGenieImage from "../../assets/GardenGenieSC.png";
 import pokedexImage from "../../assets/PokedexSC.png";
+import GardenGenie from "../../assets/GardenGenie.mp4";
+import Pokedex from "../../assets/Pokedex.mp4";
+import { useState } from "react";
 
 const ProductList = () => {
+
+
+  const [showVideoPopup, setShowVideoPopup] = useState(false);
+
+  const handleVideoButtonClick = () => {
+    setShowVideoPopup(true);
+  };
+
+  const handleCloseVideoPopup = () => {
+    setShowVideoPopup(false);
+  };
   return (
     <div className="pl">
       <div className="pl-texts">
@@ -15,10 +28,30 @@ const ProductList = () => {
         <div className="pl-item">
           <Product imageSrc={gardenGenieImage} />
           <p>Garden Genie was created to assist gardening novices in starting their gardening journey by providing plant recommendations based on users' needs. This project served as the final assignment during my bootcamp, completed collaboratively over four weeks by our team.</p>
+          <div>
+          <button onClick={handleVideoButtonClick}>Watch Demo</button>
+
+          </div>
+          {showVideoPopup && (
+            <div className="video-modal-overlay">
+        <div className="video-popup">
+          <video width="560" height="315" controls>
+            <source src={GardenGenie} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <button onClick={handleCloseVideoPopup}>Close</button>
         </div>
+        </div>
+      )}
+        </div>
+
+
+
+
         <div className="pl-item">
-          <Product imageSrc={pokedexImage} />
+          <Product imageSrc={pokedexImage}/>
           <p>Pokémon has always held a special place in my childhood memories. Leveraging the Pokémon API, I crafted a Pokédex, seamlessly fetching and rendering data from the API and presenting it in an engaging card format</p>
+          
         </div>
 
       </div>
