f40974465effa472dabae012a831d4b89ad34713
Saad Ashraf <saadash1268@gmail.com>
2024-01-03 15:38:11 +0000

    added pokedex video popup and button

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -9,52 +9,71 @@ import { useState } from "react";
 const ProductList = () => {
 
 
-  const [showVideoPopup, setShowVideoPopup] = useState(false);
+  const [showGardenGeniePopup, setShowGardenGeniePopup] = useState(false);
+  const [showPokedexPopup, setShowPokedexPopup] = useState(false);
 
-  const handleVideoButtonClick = () => {
-    setShowVideoPopup(true);
+  const handleGardenGenieVideoButtonClick = () => {
+    setShowGardenGeniePopup(true);
   };
 
-  const handleCloseVideoPopup = () => {
-    setShowVideoPopup(false);
+  const handleCloseGardenGenieVideoPopup = () => {
+    setShowGardenGeniePopup(false);
+  };
+
+  const handlePokedexVideoButtonClick = () => {
+    setShowPokedexPopup(true);
+  };
+
+  const handleClosePokedexVideoPopup = () => {
+    setShowPokedexPopup(false);
   };
   return (
     <div className="pl">
       <div className="pl-texts">
         <h1 className="pl-title">Projects</h1>
-        {/* <p className="pl-desc">A few projects </p> */}
       </div>
       <div className="pl-content">
         <div className="pl-item">
           <Product imageSrc={gardenGenieImage} />
-          <p>Garden Genie was created to assist gardening novices in starting their gardening journey by providing plant recommendations based on users' needs. This project served as the final assignment during my bootcamp, completed collaboratively over four weeks by our team.</p>
+          <p>
+            Garden Genie was created to assist gardening novices in starting their gardening journey by providing plant recommendations based on users' needs. This project served as the final assignment during my bootcamp, completed collaboratively over four weeks by our team.
+          </p>
           <div>
-          <button onClick={handleVideoButtonClick}>Watch Demo</button>
-
+            <button onClick={handleGardenGenieVideoButtonClick}>Watch Demo</button>
           </div>
-          {showVideoPopup && (
-            <div className="video-modal-overlay">
-        <div className="video-popup">
-          <video width="560" height="315" controls>
-            <source src={GardenGenie} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          <button onClick={handleCloseVideoPopup}>Close</button>
         </div>
+        <div className="pl-item">
+          <Product imageSrc={pokedexImage} />
+          <p>
+            Pokémon has always held a special place in my childhood memories. Leveraging the Pokémon API, I crafted a Pokédex, seamlessly fetching and rendering data from the API and presenting it in an engaging card format.
+          </p>
+          <div>
+            <button onClick={handlePokedexVideoButtonClick}>Watch Demo</button>
+          </div>
+        </div>
+      </div>
+
+      {showGardenGeniePopup && (
+        <div className="video-modal-overlay">
+          <div className="video-popup">
+            <video width="560" height="315" controls>
+              <source src={GardenGenie} type="video/mp4" />
+            </video>
+            <button onClick={handleCloseGardenGenieVideoPopup}>Close</button>
+          </div>
         </div>
       )}
+
+      {showPokedexPopup && (
+        <div className="video-modal-overlay">
+          <div className="video-popup">
+            <video width="560" height="315" controls>
+              <source src={Pokedex} type="video/mp4" />
+            </video>
+            <button onClick={handleClosePokedexVideoPopup}>Close</button>
+          </div>
         </div>
-
-
-
-
-        <div className="pl-item">
-          <Product imageSrc={pokedexImage}/>
-          <p>Pokémon has always held a special place in my childhood memories. Leveraging the Pokémon API, I crafted a Pokédex, seamlessly fetching and rendering data from the API and presenting it in an engaging card format</p>
-          
-        </div>
-
-      </div>
+      )}
     </div>
   );
 };
