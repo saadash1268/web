5005e231ccd051f2d2c586ae7c096f31ce70f13e
Saad Ashraf <saadash1268@gmail.com>
2024-02-25 16:18:23 +0000

    css changes and new about me text

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,14 +8,12 @@ const About = () => {
             <div className="a-right">
                 <h1 className="a-title">About Me</h1>
                 {/* <p className="a-sub">Change content, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt.</p> */}
-                <p className="a-desc">Change content.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt.
-                In hac habitasse platea dictumst. Duis euismod augue vel odio fermentum, vel dictum ex tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt.
-                In hac habitasse platea dictumst. Duis euismod augue vel odio fermentum, vel dictum ex tincidunt.
-                Change content, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt.
-
-                Change content.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt. In hac habitasse platea dictumst. Duis euismod augue vel odio fermentum, vel dictum ex tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt. In hac habitasse platea dictumst. Duis euismod augue vel odio fermentum, vel dictum ex tincidunt.Change content, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt.
-
-                Change content.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt. In hac habitasse platea dictumst. Duis euismod augue vel odio fermentum, vel dictum ex tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo nec urna fringilla tincidunt. In hac habitasse platea dictumst. Duis euismod augue vel odio fermentum, vel dictum ex tincidunt.</p>
+                <div className="a-desc">
+                    <p>My tech journey began with my environmental geography degree, where I used geospatial technology to map pollution trends.</p>
+                    <p>After graduating, I worked in the care sector, supporting young people with learning disabilities. Here I saw the impact of accessible technology, such as communication apps for non-verbal children, and recognised the need for inclusive design and accessibility in enhancing lives.</p>
+                    <p>Motivated to learn more about tech, I was chosen from over 3,500 applicants to join the School of Code's 16-week web development bootcamp. This intense program honed my coding skills and fostered a mindset of continuous learning, collaboration, and creative problem-solving.</p>
+                    <p>Now, as I step into the professional world, my goal is to join an organisation where I can continue to grow and apply my skills. I'm eager to contribute to projects that prioritise innovative and accessible solutions.</p>
+                    </div>
             </div>
         </div>
     )
