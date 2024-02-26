29e03b055e457c6eb6accc9cefa854cbe6e84833
Saad Ashraf <saadash1268@gmail.com>
2024-02-26 15:43:21 +0000

    updated project decsriptions making them more concise

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -81,7 +81,8 @@ const ProductList = () => {
       </div>
       <div className="content">
         <p className="description">
-        Garden Genie is an intuitive app designed to help gardening beginners embark on their green journey with ease, offering personalised plant recommendations based on individual needs like time and space availability. Developed as our final collaborative bootcamp project over an intense four-week period. Garden Genie stands out with its unique features, guiding users from plant selection to ongoing care. // need to add tech stack
+        Garden Genie helps gardening beginners on their green journey, offering personalised plant recommendations based on individual needs. This final collaborative bootcamp project, developed over four weeks, leverages React and Bootstrap for the front-end, with Node.js and MongoDB powering the back-end. It's deployed on Netlify.
+
         </p>
         <div className="project-actions">
           <a href="https://github.com/M-A-S-H-E-D6/M.A.S.H.E.D_Garden_Genie" target="_blank" rel="noopener noreferrer" className="icon-link">
@@ -107,7 +108,7 @@ const ProductList = () => {
   </div>
   <div className="content">
     <p className="description">
-    Pokémon has always been a cherished part of my childhood, inspiring me to create a digital Pokédex. Using the Pokémon API, this project involved fetching and rendering data, and presenting it in a card format. Each card showcases a Pokémon with its details, offering a visually engaging and user-friendly experience. This Pokédex not only rekindles nostalgia but also demonstrates my technical skills in API integration and front-end development.
+    Pokémon has always been a cherished part of my childhood, inspiring me to create a digital Pokédex. Using the Pokémon API, this project involved fetching and rendering data, and presenting it in a card format using HTML, CSS and JavaScript. 
     </p>
     <div className="project-actions">
       <a href="https://github.com/saadash1268/Pokedex-" target="_blank" rel="noopener noreferrer" className="icon-link">
@@ -128,8 +129,7 @@ const ProductList = () => {
   </div>
   <div className="content">
     <p className="description">
-    CodeCards is a dynamic flashcard app designed specifically for coding bootcamp students, leveraging spaced repetition to simplify and enhance the learning process. Created in a one-week collaborative effort during our sixth week at the bootcamp. It’s perfect for anyone embarking on or advancing their coding journey, offering an efficient and engaging way to absorb and retain complex coding concepts.
-    </p>
+    CodeCards is a flashcard app designed for bootcamp students to support their learning, created in a one-week collaborative effort during our sixth week at the bootcamp. This project leverages HTML, CSS, and React, with Vite significantly accelerating our development process    </p>
     <div className="project-actions">
       <a href="https://github.com/SchoolOfCode/bc14_w7_project-frontend-sorcerers-of-code" target="_blank" rel="noopener noreferrer" className="icon-link">
         <FontAwesomeIcon icon={faGithub} />
